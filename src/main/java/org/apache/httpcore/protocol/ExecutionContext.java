27b00666dda573ae5b780631f358d79f63b3e6e8
huangyy <hyy12345678@163.com>
2020-09-09 17:04:20 +0800

    first commit

---
+++ src/main/java/org/apache/httpcore/protocol/ExecutionContext.java
@@ -0,0 +1,19 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package org.apache.httpcore.protocol;
+
+/** @deprecated */
+@Deprecated
+public interface ExecutionContext {
+    String HTTP_CONNECTION = "http.connection";
+    String HTTP_REQUEST = "http.request";
+    String HTTP_RESPONSE = "http.response";
+    String HTTP_TARGET_HOST = "http.target_host";
+    /** @deprecated */
+    @Deprecated
+    String HTTP_PROXY_HOST = "http.proxy_host";
+    String HTTP_REQ_SENT = "http.request_sent";
+}
